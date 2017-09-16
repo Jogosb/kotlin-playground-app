39a72b918f5ac048a6f16de7c24a8ad5c0420215
Filip Radon <filipradon@MacBook-Pro-3.local>
2017-09-16 19:02:38 +0200

    Added Request class and used it in MainActivity to retrieve weather forecast.

--- app/src/main/java/com/filipradon/kotlinplaygroundapp/MainActivity.kt
+++ app/src/main/java/com/filipradon/kotlinplaygroundapp/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,5 +28,14 @@ class MainActivity : AppCompatActivity() {
         val forecastList: RecyclerView = find(R.id.forecastList)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(forecastItems)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
+
     }
 }
---
+++ app/src/main/java/com/filipradon/kotlinplaygroundapp/Request.kt
@@ -0,0 +1,15 @@
+package com.filipradon.kotlinplaygroundapp
+
+import android.util.Log
+import java.net.URL
+
+/**
+ * Created by filipradon on 16/09/17.
+ */
+class Request(val url: String) {
+
+    fun run() {
+        val forecastJsonString = URL(url).readText()
+        Log.d(javaClass.simpleName, forecastJsonString)
+    }
+}
