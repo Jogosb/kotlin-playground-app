507a86f0eeea98218b1ca391aa79fa1ca70356bf
Filip Radon <filipradon@MacBook-Pro-3.local>
2017-09-18 22:07:30 +0200

    Add Picasso library. Extended item layout. List is now clickable and looks much better

--- app/src/main/java/com/filipradon/kotlinplaygroundapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/filipradon/kotlinplaygroundapp/ui/adapters/ForecastListAdapter.kt
@@ -1,26 +1,56 @@
 package com.filipradon.kotlinplaygroundapp.ui.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.filipradon.kotlinplaygroundapp.R
+import com.filipradon.kotlinplaygroundapp.domain.model.Forecast
 import com.filipradon.kotlinplaygroundapp.domain.model.ForecastList
+import com.filipradon.kotlinplaygroundapp.ui.utils.ctx
+import com.squareup.picasso.Picasso
+import org.jetbrains.anko.find
 
 /**
  * Created by filipradon on 16/09/17.
  */
-class ForecastListAdapter(val weekForecast: ForecastList): RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: OnItemClickListener)
+    : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    interface OnItemClickListener {
+        operator fun invoke(forecast: Forecast)
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(weekForecast[position]) {
-            holder.textView.text = "$date - $description - $high/$low"
-        }
+        holder.bindForecast(weekForecast[position])
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(TextView(parent.context))
+        val view = LayoutInflater.from(parent.ctx).inflate(R.layout.item_forecast, parent, false)
+        return ViewHolder(view, itemClick)
     }
 
     override fun getItemCount() = weekForecast.size
 
-    class ViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+    class ViewHolder(view: View, val itemClick: OnItemClickListener) : RecyclerView.ViewHolder(view) {
+
+        private val iconView = view.find<ImageView>(R.id.icon)
+        private val dateView = view.find<TextView>(R.id.date)
+        private val descriptionView = view.find<TextView>(R.id.description)
+        private val maxTemperatureView = view.find<TextView>(R.id.maxTemperature)
+        private val minTemperatureView = view.find<TextView>(R.id.minTemperature)
+
+        fun bindForecast(forecast: Forecast) {
+            with(forecast) {
+                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
+                dateView.text = date
+                descriptionView.text = description
+                maxTemperatureView.text = "${high}º"
+                minTemperatureView.text = "${low}º"
+                itemView.setOnClickListener { itemClick(this) }
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/filipradon/kotlinplaygroundapp/ui/utils/ViewExtensions.kt
@@ -0,0 +1,10 @@
+package com.filipradon.kotlinplaygroundapp.ui.utils
+
+import android.content.Context
+import android.view.View
+
+/**
+ * Created by filipradon on 18/09/17.
+ */
+val View.ctx: Context
+    get() = context
