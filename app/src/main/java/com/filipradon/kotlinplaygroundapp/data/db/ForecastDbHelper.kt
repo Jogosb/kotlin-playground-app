8b99a9b7825a52d45dded327807db921f9317c1c
Filip Radon <filipradon@MacBook-Pro-3.local>
2017-09-23 19:09:29 +0200

    Added anko extension for sqlite. Created two tables for the forecast data.

---
+++ app/src/main/java/com/filipradon/kotlinplaygroundapp/data/db/ForecastDbHelper.kt
@@ -0,0 +1,49 @@
+package com.filipradon.kotlinplaygroundapp.data.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import com.filipradon.kotlinplaygroundapp.ui.ForecastApplication
+import org.jetbrains.anko.db.AUTOINCREMENT
+import org.jetbrains.anko.db.INTEGER
+import org.jetbrains.anko.db.ManagedSQLiteOpenHelper
+import org.jetbrains.anko.db.PRIMARY_KEY
+import org.jetbrains.anko.db.TEXT
+import org.jetbrains.anko.db.createTable
+import org.jetbrains.anko.db.dropTable
+
+/**
+ * Created by filipradon on 23/09/17.
+ */
+class ForecastDbHelper(ctx: Context = ForecastApplication.instance) : ManagedSQLiteOpenHelper(ctx,
+        ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
+
+    companion object {
+        val DB_NAME = "forecast.db"
+        val DB_VERSION = 1
+        val instance by lazy { ForecastDbHelper() }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable(CityForecastTable.NAME, true,
+                CityForecastTable.ID to INTEGER + PRIMARY_KEY,
+                CityForecastTable.CITY to TEXT,
+                CityForecastTable.COUNTRY to TEXT)
+
+        db.createTable(DayForecastTable.NAME, true,
+                DayForecastTable.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                DayForecastTable.DATE to INTEGER,
+                DayForecastTable.DESCRIPTION to TEXT,
+                DayForecastTable.HIGH to INTEGER,
+                DayForecastTable.LOW to INTEGER,
+                DayForecastTable.ICON_URL to TEXT,
+                DayForecastTable.CITY_ID to INTEGER)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.dropTable(CityForecastTable.NAME, true)
+        db.dropTable(DayForecastTable.NAME, true)
+        onCreate(db)
+    }
+
+
+}
---
+++ app/src/main/java/com/filipradon/kotlinplaygroundapp/data/db/Tables.kt
@@ -0,0 +1,22 @@
+package com.filipradon.kotlinplaygroundapp.data.db
+
+/**
+ * Created by filipradon on 23/09/17.
+ */
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
